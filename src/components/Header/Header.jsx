47c970eafb4ba29bf73eb82e1e23b1ccbcbda720
fireaspectII <smalldangernight@gmail.com>
2026-04-02 11:20:21 +0500

    header qushildi

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,23 @@ import "./Header.css"
 function Header() {
   return (
     <header className='header'>
-        <ul>
-          <li><a href=""></a></li>
-        </ul>
+      <div className='header-container'>
+        <div className='logo'>
+          <h1><a href="/">ShopHub</a></h1>
+        </div>
+        <nav className='nav-menu'>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </nav>
+        <div className='header-actions'>
+          <input type="search" placeholder="Search products..." className='search-bar' />
+          <button className='cart-btn'>Cart (0)</button>
+        </div>
+      </div>
     </header>
   )
 }
